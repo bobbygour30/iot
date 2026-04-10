253627082494164060d6b1dec6a6a730a92723a5
Rajat <rajat804@gmail.com>
2026-04-10 17:49:59 +0530

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom'
-import RegisterPage from './pages/auth/RegisterPage'
-import LoginPage from './pages/auth/LoginPage'
-import DashboardLayout from './components/Layout/DashboardLayout'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/auth/RegisterPage";
+import LoginPage from "./pages/auth/LoginPage";
+import DashboardLayout from "./components/Layout/DashboardLayout";
+import Equipment from "./pages/Equipment";
+import Dashboard from "./pages/Dashboard";
+import Indices from "./pages/Indices";
+import Reports from "./pages/Reports";
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/dashboard' element={<DashboardLayout />} />
-          <Route path='/' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage />} />
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path="/dashboard" element={<DashboardLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="equipment" element={<Equipment />} />
+            <Route path="indices" element={<Indices />} />
+            <Route path="reports" element={<Reports />} />
+
+          </Route>
         </Routes>
       </Router>
-      
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
